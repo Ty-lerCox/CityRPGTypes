30ac2b0dc01a6981718a412701dccbaed5342387
Ty-lerCox <tycox94@gmail.com>
2021-05-08 14:28:55 -0400

    removed js files

--- item.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ItemType = void 0;
-var ItemType;
-(function (ItemType) {
-    ItemType[ItemType["Computer"] = 0] = "Computer";
-})(ItemType = exports.ItemType || (exports.ItemType = {}));
--- property.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PropertyType = void 0;
-var PropertyType;
-(function (PropertyType) {
-    PropertyType[PropertyType["Lot"] = 0] = "Lot";
-})(PropertyType = exports.PropertyType || (exports.PropertyType = {}));
